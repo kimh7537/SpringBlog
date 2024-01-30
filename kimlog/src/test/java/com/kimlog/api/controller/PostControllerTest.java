77ee32dd46a7db71fc16b1167996fe8f494c0eb0
kimh7537 <kimh7537@gmail.com>
2024-01-30 19:32:29 +0900

    데이터 검증 예외 처리 해보기

--- kimlog/src/test/java/com/kimlog/api/controller/PostControllerTest.java
+++ kimlog/src/test/java/com/kimlog/api/controller/PostControllerTest.java
@@ -26,7 +26,7 @@ class PostControllerTest {
     void test() throws Exception {
 
         //expected
-        mockMvc.perform(post("/posts")
+        mockMvc.perform(post("/posts") //post요청
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\"}")
                 )
@@ -47,8 +47,10 @@ class PostControllerTest {
 //                        .content("{\"title\": \"\", \"content\": \"내용입니다.\"}")
                           .content("{\"title\": null, \"content\": \"내용입니다.\"}")
                 )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title").value("타이틀을 입력해주세요."))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
                 .andDo(print());
     }
 
